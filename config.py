8dc86151c646bbfa46b4d03b277bad72545e0dd9
Ruslan-1502 <82501852+Ruslan-1502@users.noreply.github.com>
2025-03-21 17:17:18 +0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# import os
-
-# BOT_TOKEN = '5854308493:AAGVwnJ4KgN_1CByiwyw-Yz68GZN7426Hhg'
-# WEBHOOK_URL = f'https://dain2.herokuapp.com/webhook/5854308493:AAGVwnJ4KgN_1CByiwyw-Yz68GZN7426Hhg'
-# WEBAPP_HOST = '0.0.0.0'
-# WEBAPP_PORT = os.environ.get('PORT')
-# WEBHOOK_PATH = '/webhook/5854308493:AAGVwnJ4KgN_1CByiwyw-Yz68GZN7426Hhg'
